49361808bed438848d8d65c72676fa43b8bf55ed
qgqqq <89883538+qgqqq@users.noreply.github.com>
2026-01-16 18:47:52 +0800

    MakeCode 项目的初始文件

---
+++ tilemap.g.ts
@@ -0,0 +1,14 @@
+// 自动生成的代码。请勿编辑。
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "tile1":return tile1;
+        }
+        return null;
+    })
+
+}
+// 自动生成的代码。请勿编辑。
